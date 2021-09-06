1eac9c322b8f8ee6b6d6aee2154c8983ca84c4da
todor ivanov <todorivanov066@gmail.com>
2021-09-06 21:54:42 +0300

    silenced compilation warnings

--- lenslocked.com/main.go
+++ lenslocked.com/main.go
@@ -8,12 +8,12 @@ import (
 
 func home(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	_, _ = fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
 func contact(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "To get in touch, please send an email "+
+	_, _ = fmt.Fprint(w, "To get in touch, please send an email "+
 		"to <a href=\"mailto:support@lenslocked.com\">"+
 		"support@lenslocked.com</a>.")
 }
@@ -22,5 +22,5 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	http.ListenAndServe(":3000", r)
+	_ = http.ListenAndServe(":3000", r)
 }
